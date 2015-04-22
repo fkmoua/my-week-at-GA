d0cb8b8e4943c1955fbefbd064f8aba031a6ca3a
Your Name <mouafancy@gmail.com>
2015-04-22 15:56:26 -0700

    updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -25,8 +25,8 @@ Rails.application.routes.draw do
 
   # get '/login'     => 'sessions#new'
   root 'sessions#new'
-  post '/login'    => 'sessions#create'
-  delete '/logout' => 'sessions#destroy'
+  post 'login'    => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
